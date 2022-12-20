fa96acbe97fb4c48efce1674a5dcf5dedf1ac7e5
sangminby <113090480+sangminby@users.noreply.github.com>
2022-12-20 12:14:45 +0900

    삭제

---
+++ TodoMiniProject/src/main/java/com/todo/todominiproject/controller/TodoDeleteController.java
@@ -0,0 +1,23 @@
+package com.todo.todominiproject.controller;
+
+import com.todo.todominiproject.service.TodoDeleteService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TodoDeleteController {
+
+    @Autowired
+    private TodoDeleteService todoDeleteService;
+
+    @GetMapping("/todo/delete")
+    public String deleteTodo(@Param("tno") int tno) {
+
+        todoDeleteService.deleteTodo(tno);
+
+        return "redirect:/todo/list";
+    }
+
+}
--- TodoMiniProject/src/main/java/com/todo/todominiproject/service/TodoDeleteService.java
+++ TodoMiniProject/src/main/java/com/todo/todominiproject/service/TodoDeleteService.java
@@ -1,8 +1,11 @@
 package com.todo.todominiproject.service;
 
+import com.todo.todominiproject.entity.Todo;
 import com.todo.todominiproject.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class TodoDeleteService {
@@ -11,6 +14,20 @@ public class TodoDeleteService {
     private TodoRepository todoRepository;
 
 
-    public int
+    public int deleteTodo(int tno) {
+
+        Todo todo = todoRepository.findById(tno).get();
+
+        int result = todoRepository.deleteByTno(tno);
+
+        if(result>0 && todo.getPhoto()!=null){
+
+            File delFile = new File(new File("").getAbsolutePath(), "photo"+File.separator+todo.getPhoto());
+
+        }
+
+        return result;
+
+    }
 
 }

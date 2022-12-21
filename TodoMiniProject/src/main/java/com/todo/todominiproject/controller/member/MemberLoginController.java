bd63c3f9a2080cdd1e0924932ac2dd707df6470e
sangminby <113090480+sangminby@users.noreply.github.com>
2022-12-21 13:05:16 +0900

    login

--- TodoMiniProject/src/main/java/com/todo/todominiproject/controller/member/MemberLoginController.java
+++ TodoMiniProject/src/main/java/com/todo/todominiproject/controller/member/MemberLoginController.java
@@ -2,13 +2,22 @@ package com.todo.todominiproject.controller.member;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member/login")
 public class MemberLoginController {
 
-    @GetMapping("/member/login")
+    @GetMapping
     public void login() {
 
+    }
+
+    @PostMapping
+    public String afterLogin() {
+
+        return "redirect:/todo/list";
     }
 
 }
